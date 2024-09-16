bf93881c24c92a2040d20c691be4e192267009aa
Jack Moore <jack.moore314@gmail.com>
2024-09-15 19:18:27 -0500

    add favicon

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Rachel & Jack",
+    short_name: "R&J",
+    description: "Rachel & Jack's wedding website",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#000",
+    icons: [
+      {
+        src: "/favicon/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
