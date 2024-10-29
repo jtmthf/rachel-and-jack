4e0cf64d7b9bfc75e4961d76323f2e36fd931f22
Jack Moore <8162598+jtmthf@users.noreply.github.com>
2024-10-28 21:49:03 -0500

    engagement photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import engagement from "./engagement.png";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Image
+        alt="Engagement"
+        src={engagement}
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+      />
     </main>
   );
 }
