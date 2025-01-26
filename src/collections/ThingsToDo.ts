5772066ee7ee22b1e37e7c16a3dabc4f6791991b
Jack Moore <8162598+jtmthf@users.noreply.github.com>
2025-01-25 19:39:30 -0600

    revalidate things to do

--- src/collections/ThingsToDo.ts
+++ src/collections/ThingsToDo.ts
@@ -1,4 +1,38 @@
-import { CollectionConfig } from 'payload';
+import type { ThingsToDo as ThingsToDoProps } from '@/payload-types';
+import { revalidatePath } from 'next/cache';
+import {
+  CollectionAfterChangeHook,
+  CollectionAfterDeleteHook,
+  CollectionConfig,
+} from 'payload';
+
+const revalidateChange: CollectionAfterChangeHook<ThingsToDoProps> = ({
+  doc,
+}) => {
+  revalidatePath('/things-to-do');
+
+  doc.category?.forEach((category) => {
+    if (typeof category === 'object') {
+      revalidatePath(`/things-to-do/${category.slug}`);
+    }
+  });
+
+  return doc;
+};
+
+const revalidateDelete: CollectionAfterDeleteHook<ThingsToDoProps> = ({
+  doc,
+}) => {
+  revalidatePath('/things-to-do');
+
+  doc.category?.forEach((category) => {
+    if (typeof category === 'object') {
+      revalidatePath(`/things-to-do/${category.slug}`);
+    }
+  });
+
+  return doc;
+};
 
 export const ThingsToDo: CollectionConfig = {
   slug: 'things-to-do',
@@ -33,4 +67,8 @@ export const ThingsToDo: CollectionConfig = {
       required: true,
     },
   ],
+  hooks: {
+    afterChange: [revalidateChange],
+    afterDelete: [revalidateDelete],
+  },
 };
