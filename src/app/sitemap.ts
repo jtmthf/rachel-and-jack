dccff7bf1e5b4bd69e87f8602c7427627fa4002a
Jack Moore <8162598+jtmthf@users.noreply.github.com>
2025-05-03 13:29:13 -0500

    feat(sitemap): enhance sitemap generation with dynamic page fetching

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,31 @@
 import { SITE_ORIGIN } from '@/lib/contants';
+import configPromise from '@payload-config';
 import { MetadataRoute } from 'next';
+import { getPayload } from 'payload';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const dynamicParams = true;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const payload = await getPayload({ config: configPromise });
+  const pages = await payload.find({
+    collection: 'pages',
+    draft: false,
+    limit: 1000,
+    overrideAccess: false,
+    pagination: false,
+    where: {
+      _status: {
+        equals: 'published',
+      },
+    },
+    select: {
+      slug: true,
+      updatedAt: true,
+    },
+  });
+
   return [
     {
       url: SITE_ORIGIN,
@@ -15,17 +39,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 0.8,
     },
-    {
-      url: `${SITE_ORIGIN}/things-to-do`,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.8,
-    },
-    {
-      url: `${SITE_ORIGIN}/faq`,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.8,
-    },
+    ...pages.docs.map(
+      (page) =>
+        ({
+          url: `${SITE_ORIGIN}/${page.slug}`,
+          lastModified: new Date(page.updatedAt),
+          changeFrequency: 'daily',
+          priority: 0.8,
+        }) satisfies MetadataRoute.Sitemap[number],
+    ),
   ];
 }
